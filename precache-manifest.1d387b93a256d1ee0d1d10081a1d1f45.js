27c568b82d92da92a479d79ec3cfd22533c9e43b
Mohammad Imran Hossen <imranhosein.cse@gmail.com>
2022-02-16 17:22:48 +0600

    Updates

---
+++ precache-manifest.1d387b93a256d1ee0d1d10081a1d1f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e869fa3696eed53a8ba9d69f510acc4d",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "60127c70bb438f463a83",
+    "url": "/birthday-reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "3bdc1d8518940277ff32",
+    "url": "/birthday-reminder/static/js/2.ab0cd556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.ab0cd556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60127c70bb438f463a83",
+    "url": "/birthday-reminder/static/js/main.e8362c8a.chunk.js"
+  },
+  {
+    "revision": "a1ff553ac3d675eebff3",
+    "url": "/birthday-reminder/static/js/runtime-main.ed31b697.js"
+  }
+]);
